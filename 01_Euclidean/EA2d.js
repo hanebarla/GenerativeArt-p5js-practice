5c205ba231fbff6e8bd67bd3982f85a8b0440512
hanebarla <barla.wings@gmail.com>
2021-02-27 16:13:42 +0900

    decide file construction

---
+++ 01_Euclidean/EA2d.js
@@ -0,0 +1,9 @@
+function setup(){
+    let EA2d = createCanvas(400, 400);
+    background(240);
+    EA2d.parent("01-EA2d");
+}
+
+function draw(){
+    ellipse(50, 50, 80, 80);
+}
---
+++ 01_Euclidean/EA3d.js
@@ -0,0 +1,9 @@
+function setup(){
+    let EA3d = createCanvas(600, 600);
+    background(240);
+    EA3d.parent("01-EA3d");
+}
+
+function draw(){
+    ellipse(50, 70, 80, 80);
+}
